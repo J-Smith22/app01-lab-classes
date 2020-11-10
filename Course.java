7c75f06ee1fa7de0cc51d6b168059baceaca7191
Josh Smith <72292757+IRI22@users.noreply.github.com>
2020-11-10 21:45:13 +0000

    Initial commit

---
+++ Course.java
@@ -0,0 +1,27 @@
+
+/**
+ * This class will store information about the university course.
+ * Courses have an official title and a UCAS code.
+ *
+ * @author Joshua Smith
+ * @version (a version number or a date)
+ */
+public class Course
+{
+    // instance variables - replace the example below with your own
+    private String Title;
+    
+    private String codeNo;
+
+    /**
+     * Constructor for objects of class Course it
+     * stores the title and course No
+     */
+    public Course(String Title, String CodeNo)
+    {
+        this.Title = Title;
+        this.codeNo = codeNo;
+    }
+
+
+}
